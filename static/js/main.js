ae295fd5ed87b2c976694c72042a487fb04929cf
Tanel Treuberg <tanetre@gmail.com>
2023-02-13 15:37:46 +0200

    onload functionality

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,5 @@
 $(window).on("load", function(e) {
+    $(".body").each()
+    $(".content").text("Still indexing...").fadeIn(1000)
     $(".loaded").attr("src", "static/light-mode.gif").fadeIn(1000)
 });
