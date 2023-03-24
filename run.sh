a8de1ce1fb9a406ebe04c483d8853a9d16a5c910
Tanel Treuberg <tanetre@gmail.com>
2023-03-24 02:19:14 +0200

    run.sh had double --name tags

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 # #!/usr/bin/env bash
 docker build -t nginx_server . -f Dockerfile
-docker run --name web-container --rm -i -d -v $(pwd):/usr/share/nginx/html -p 8080:80 --name localpage nginx_server
+docker run --rm -i -d -v $(pwd):/usr/share/nginx/html -p 8080:80 --name localpage nginx_server
