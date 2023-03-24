abf4ebbffd7ef0d4020780c53e07ab2d019baa9e
Tanel Treuberg <tanetre@gmail.com>
2023-03-24 16:43:25 +0200

    Parse data from URL and apply to selectors

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,79 @@
-$(window).on("load", function(e) {
-    var elements = ['s-content', 's-0', 's-1', 's-loaded'];
-    $(".s-content").html("Still indexing")//.fadeIn(1000)
-    $(".s-loaded").attr("src", "static/light-mode.gif")//.fadeIn(1000)
+/**
+  * Dynamically add (or remove) a "Deselect All" pseudo-option
+  */
 
-    $(".chosen-select").chosen({
-        no_results_text: "Oops, nothing found!",
-        max_shown_results: "30",
-        width: "100%",
+function slimSelectDeselectAll(info) {
+    var curData = this.data.data;
+    var selected_count = info.length;
+    var has_deselect_button = curData.filter(v => v.value === '' || v.value === 'Deselect all').length > 0;
+    var wants_deselect = this.selected().includes("Deselect all") || this.selected().includes("");
+ 
+    if (wants_deselect) {
+       this.set([]);
+       this.setData(curData.filter(v => v.value !== '' && v.value !== 'Deselect all' ));
+       return;
+    }
+ 
+    if (selected_count && !has_deselect_button) {
+       curData.unshift({value: "", text: "Deselect all"});
+       this.setData(curData);
+    }
+}
+
+function paramsToObject(entries) {
+    const result = {}
+    var values = []
+    var multiples = "action"
+    for (const [key, value] of entries) { // each 'entry' is a [key, value] tupple
+        if (key === multiples) {
+            values.push(value)
+        } else {
+            result[key] = value;
+        }            
+    }
+    result[multiples] = values
+    return result;
+}
+
+var sel;
+var params
+$(window).on("load", function(e) {
+    url = window.location.search
+    params = paramsToObject(new URLSearchParams(url).entries())
+    console.log(params)
+
+    var elements = ['s2a-content', 's2a-0', 's2a-1', 's2a-loaded', 's2a-aabtn'];
+    $(".s2a-content").html("Still indexing")
+    $(".s2a-loaded").attr("src", "static/light-mode.gif")
+
+    // $(".chosen-select").chosen({
+    //     no_results_text: "Oops, nothing found!",
+    //     max_shown_results: "30",
+    //     width: "100%",
+    // })
+
+    action_selector = new SlimSelect({
+        select: "#itemdrop-0",
+        events: {
+            afterChange: (newVal) => {
+                console.log(newVal)
+            }
+        },
+        settings: {
+            placeholderText: "Your turn"
+        }
     })
+
+    new SlimSelect({
+        select: "#itemdrop-1",
+        events: {
+            afterChange: (newVal) => {
+                console.log(newVal[0])
+            }
+        },
+    })
+
+    action_selector.setSelected(params["action"])
 
     function showElement(elem, time) {
         setTimeout(() => {
@@ -22,16 +88,21 @@ $(window).on("load", function(e) {
     var count = 1;
     setInterval(function() {
         count++;
-        $(".s-content").html("Still indexing" + new Array(count % 5).join('.'));
+        $(".s2a-content").html("Still indexing" + new Array(count % 5).join('.'));
     }, 1000);
 });
 
 $(document).ready(function() {
-    $(document).on('change', '#queryform', function(e) {
-        // This function grabs data from form and sends it to Flask
-        e.preventDefault(); // Prevent refresh
-        form = $(this).serialize() // Converts form data to URL query form
-        // console.log(`${document.location.origin}?${form}`)
-        console.log(form)
-    });
+    // $(document).on('change', '#queryform', function(e) {
+    //     // This function grabs data from form and sends it to Flask
+    //     e.preventDefault(); // Prevent refresh
+    //     form = $(this).serialize() // Converts form data to URL query form
+    //     // console.log(`${document.location.origin}?${form}`)
+    //     console.log(form)
+    // });
+    
+    $(".s2a-aabtn").click(function() {
+        action_selector.setSelected([])
+        // action_selector.search("Open")
+    })
 });
