7c0d96ddc17243d5678bfdd1c907061d7b9dca80
Tanel Treuberg <tanetre@gmail.com>
2023-04-09 12:47:15 +0300

    Refactoring and optimisations, also minor documentation

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,7 @@
 function paramsToObject(entries) {
     const result = {}
     var values = []
-    var multiples = "action"
+    var multiples = "action" // TODO: figure out a way to deal with a list of the same values m = ["a", "b"], use m.includes(key) @L6
     for (const [key, value] of entries) { // each 'entry' is a [key, value] tupple
         if (key === multiples) {
             values.push(value)
@@ -42,14 +42,17 @@ $(window).on("load", function(e) {
     // }
     // console.log(query_params)
     query_params = {}
+    // Find all elements that need to be hidden
     var found = document.getElementsByClassName("s2a")
     var elements = []
     for (var i = 0; i < found.length; i++) {
         elements.push(found[i].className.split(" ")[1])
     }
+    // Initialise object data
     $(".content").html("Still indexing")
     $(".clipimg").attr("src", "static/copyit.png")
 
+    // Initialise Slim and Chosen selectors
     action_selector = new SlimSelect({
         select: "#itemdrop-0",
         events: {
@@ -59,7 +62,6 @@ $(window).on("load", function(e) {
         },
         settings: {
             placeholderText: "Your turn",
-            allowDeselect: true
         }
     })
 
@@ -67,42 +69,43 @@ $(window).on("load", function(e) {
         select: "#itemdrop-1",
         events: {
             afterChange: () => {
-                // console.log(subject_selector.getSelected())
                 customURL()
             }
         },
     })
 
-    // Set selector values to black
-    var ssMain = $(".ss-main");
-    var ssValues = ssMain.find('.ss-values');
-    ssValues.bind("DOMNodeInserted", function () {
-        ssValues.find("div").each(function() {
-            // console.log(this.className)
-            if (this.className == "ss-value")
-            $(this).css('background-color', 'black');
-        });
-    });
-
-    // action_selector.setSelected(query_params["actions"])
-    // subject_selector.setSelected(query_params["subject"])
-    action_selector.setSelected(params.getAll("action"))
-    subject_selector.setSelected(params.get("subject"))
-
     chosen = $(".chosen-select")
-    chosen.val(params.getAll("items")) // Set Chosen dropdown value
-    chosen.trigger("chosen:updated")
-
-
     chosen.chosen({
         no_results_text: "Oops, nothing found!",
         max_shown_results: "30",
         width: "100%"
     })
 
+    // Set Slim selector values color to black
+    var ssMain = $(".ss-main");
+    var ssValues = ssMain.find('.ss-values');
+    ssValues.bind("DOMNodeInserted", function () {
+        ssValues.find("div").each(function() {
+            if (this.className == "ss-value")
+            $(this).css('background-color', 'black');
+        });
+    });
+
+    // Initialise Slim and Chosen selectors with URL parameters
+    action_selector.setSelected(params.getAll("action"))
+    subject_selector.setSelected(params.get("subject"))
+
+    chosen.val(params.getAll("items")) // Set Chosen dropdown value
+    chosen.trigger("chosen:updated")
+
+    // Set Chosen selector event listener
+    $(".chosen-select").on("change", function() {
+        customURL()
+    })
+
+    // Initialise clipboard element
     var btn = document.getElementById('clippy');
     var clipboard = new ClipboardJS(btn);
-
     clipboard.on('success', function (e) {
         $(".clipimg").attr("src", "static/copydone.png")
         setTimeout(function() {
@@ -110,13 +113,6 @@ $(window).on("load", function(e) {
         }, 1000)
     });
 
-    $(".chosen-select").on("change", function() {
-        var chosen = $(this);
-        // var initialised_elements = chosen.find(":selected").text()
-        // console.log(initialised_elements)
-        // console.log(chosen.val()) //Print out initialised values
-        customURL()
-    })
     // Create a URL instance when no interaction from the user
     customURL()
 
@@ -147,25 +143,25 @@ $(window).on("load", function(e) {
         $(".content").html("Still indexing" + new Array(count % 5).join('.'));
     }, 1000);
 
+    // Custom URL constructor
     function customURL() {
+        // Set query parameters dict data
         query_params["action"] = action_selector.getSelected()
         query_params["subject"] = subject_selector.getSelected()
+        query_params["items"] = chosen.val().length > 0 ? chosen.val() : []
 
-        // Construct the URL
+        // Initialise URL constructor
         const p = new URLSearchParams();
-        for (var i = 0; i < query_params["action"].length; i++) {
-            p.append("action", query_params["action"][i]);
-        }
-        p.append("subject", query_params["subject"])
-
-        // Append from chosen
-        if (chosen.val().length > 0) {
-            query_params["items"] = chosen.val()
-            for (var i = 0; i < query_params["items"].length; i++) {
-                p.append("items", query_params["items"][i]);
+        // Append data to it, works with keys have single and multiple values
+        multikeys = Object.keys(query_params)
+        for (var index = 0; index < multikeys.length; index++) {
+            var key = multikeys[index]
+            for (var i = 0; i < query_params[key].length; i++) {
+                p.append(key, query_params[key][i]);
             }
         }
 
+        // Construct URL and update the respective element
         var new_url = new URL(`${url.origin}${url.pathname}?${p}`)
         $(".page_link").text(new_url)
     }
@@ -173,7 +169,6 @@ $(window).on("load", function(e) {
 });
 
 $(document).ready(function() {
-    // console.log(query_params)
     $(".aabtn").click(function() {
         action_selector.setSelected([])
         subject_selector.setSelected([])
