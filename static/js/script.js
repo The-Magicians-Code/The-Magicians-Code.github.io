2f2a91a8524d309b442a51f7a499206f5cfe65bb
Tanel Treuberg <tanetre@gmail.com>
2023-12-27 16:16:54 +0200

    Major reworks

---
+++ static/js/script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const animatedText = document.getElementById('animated-text');
+    let degree = 0;
+
+    function animateGradient() {
+        setInterval(() => {
+            degree = (degree + 1) % 360;
+            animatedText.style.backgroundImage = `linear-gradient(${degree}deg, blue, red)`;
+        }, 20); // Adjust the interval for speed
+    }
+
+    animateGradient();
+});
